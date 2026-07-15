7cbf0f814862a4d778b722135bee7eb341b0c3cd
vijay <vijay@gmail.com>
2026-07-15 23:31:28 +0530

    Add production configs

--- docappsystem/settings.py
+++ docappsystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=u!!em!u$#9d=ew1uzeq&=90w(%62nx5b)9j66kbhh2*ee__il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
